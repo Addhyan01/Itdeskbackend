5d6b4bd8a5abffc1d5f50e22be60ec03aee86e49
Addhyan Kumar <addhyankumar@Addhyans-MacBook-Air.local>
2026-03-11 16:17:36 +0530

    here we delete all models scheema and routes and after cleaning we continue from skrach

--- server.js
+++ server.js
@@ -4,23 +4,17 @@ const express = require("express")
 const cors = require("cors")
 const mongoose = require("mongoose")
 
-const ticketRoutes = require("./routes/ticketRoutes")
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
-app.use("/uploads", express.static("uploads"))
-const aiRoutes = require("./routes/aiRoutes")
 
 mongoose.connect(process.env.MONGO_URI)
 .then(()=>console.log("MongoDB Connected"))
 .catch(err=>console.log(err))
 
-app.use("/api/tickets",ticketRoutes)
-app.use("/api/ai", aiRoutes)
 
-// app.use("/api/ai",aiRoutes)
 
 app.get("/",(req,res)=>{
 res.send("Helpdesk API running")
